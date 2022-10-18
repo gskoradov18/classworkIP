e68b4863d51fe05e7493971cf221a8382c75b91c
gskoradov18 <gskoradov18@codingburgas.bg>
2022-10-18 16:20:26 +0300

    2nd task

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     int i;
+    int a;
+    boolean b;
     private Main(){
-        i=5;
-        System.out.println( "Constructor is called");
+        i =5;
+        System.out.println("Constructor is called");
     }
-    public static void main(String[] args){
-        Main obj=new Main();
-        System.out.println("Value of i:"+obj.i);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Main ojc = new Main();
+        Main obj = new Main();
+        System.out.println("Vlue of i: " + ojc.i);
+        System.out.println("a = " + obj.a);
+        System.out.println("b = " + obj.b);
     }
 }
